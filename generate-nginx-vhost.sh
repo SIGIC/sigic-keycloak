c22cf9cde03e7cc7ffa514df384baa592e1bdd7f
cesarbenjamin <mathereall@gmail.com>
2025-05-14 12:09:02 -0600

    fix(nginx) simplify CORS handling and update variable usage in vhost template

--- generate-nginx-vhost.sh
+++ generate-nginx-vhost.sh
@@ -7,8 +7,8 @@ CORS_ORIGIN=$2
 
 # CORS_ORIGIN es el origen que se permite para las peticiones CORS, por ahora no aplica ningún cambio
 
-if [ -z "$DOMAIN" ] || [ -z "$CORS_ORIGIN" ]; then
-  echo "Uso: $0 dominio.com https://origen-cors.com|*"
+if [ -z "$DOMAIN" ]; then
+  echo "Uso: $0 dominio.com"
   exit 1
 fi
 
@@ -19,8 +19,7 @@ fi
 TEMPLATE="./nginx/templates/nginx-vhost.conf.template"
 OUTPUT="./nginx/vhost.d/${DOMAIN}"
 
-export CORS_ORIGIN
-
-envsubst < "$TEMPLATE" > "$OUTPUT"
+# NO usamos envsubst para no romper las variables de nginx
+cp "$TEMPLATE" "$OUTPUT"
 
 echo "✅ Archivo generado: $OUTPUT"
